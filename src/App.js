c26d2cc7aced7699fa4e111180fc83d4be1c9dc4
kelvanince1 <incekelvan@gmail.com>
2018-01-31 13:00:35 -0600

    Add transition library and animation effects

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Transition from 'react-transition-group/Transition';
+
 import './App.css';
+import logo from './logo.svg';
+import Modal from "./components/Modal/Modal";
 
 class App extends Component {
+  state = {
+    modalIsOpen: false,
+    showBlock: false
+  }
+
+  showModal = () => {
+    this.setState({
+      modalIsOpen: true,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({
+      modalIsOpen: false
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +30,35 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Flash! Bam! Alakazam!</h1>
         </header>
-        <p className="App-intro">
-          Lets see what we can do
-        </p>
+        <button className='Button' onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={600}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log('OnEnter')}
+          onEntering={() => console.log('OnEntering')}
+          onEntered={() => console.log('OnEntered')}
+          onExit={() => console.log('OnExit')}
+          onExiting={() => console.log('OnExiting')}
+          onExited={() => console.log('OnExited')}
+        >
+          {state =>
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                transition:  'opacity 1s ease-out',
+                opacity: state === 'exiting' || state === 'entering' ? 0 : 1
+              }}>
+            </div>
+          }
+        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        <button className="Button" onClick={this.showModal}>Open Modal</button>
         <div>
           <nav>
             <ul>
